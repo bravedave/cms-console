4d497cf1a5d9034bafcece55d49ef5aaec35e4b3
Dave Bray <david@brayworth.com.au>
2021-06-16 12:57:21 +1000

    migrating console_tenants

---
+++ src/console/dao/console_tenants.php
@@ -0,0 +1,38 @@
+<?php
+/*
+ * David Bray
+ * BrayWorth Pty Ltd
+ * e. david@brayworth.com.au
+ *
+ * MIT License
+ *
+*/
+
+namespace cms\console\dao;
+
+use cms\console\db;
+use dao\_dao;
+
+class console_tenants extends _dao {
+  public function getVersion() {
+    $sql = sprintf(
+      'SELECT MAX(version) v FROM `%s`',
+      $this->db_name()
+
+    );
+
+    if ($res = $this->Result($sql)) {
+      if ($dto = $res->dto()) {
+        return ((int)$dto->v);
+      }
+    }
+
+    return (0);
+  }
+
+  public function UpdateByID($a, $id) {
+    $a['version'] = $this->getVersion() + 1;
+    return (parent::UpdateByID($a, $id));
+  }
+
+}
---
+++ src/console/dao/db/console_tenants.php
@@ -0,0 +1,50 @@
+<?php
+/*
+ * David Bray
+ * BrayWorth Pty Ltd
+ * e. david@brayworth.com.au
+ *
+ * MIT License
+ *
+*/
+
+namespace dao;
+
+$dbc = \sys::dbCheck('console_tenants' );
+$dbc->defineField( 'TenantID', 'bigint');
+$dbc->defineField( 'ContactID', 'bigint');	// console contact id
+$dbc->defineField( 'ContactIDs', 'text');		// console contact ids
+$dbc->defineField( 'FileAs', 'varchar', 200);
+$dbc->defineField( 'Mobile', 'varchar', 50);
+$dbc->defineField( 'Email', 'varchar', 50);
+$dbc->defineField( 'Street', 'varchar', 100);
+$dbc->defineField( 'City', 'varchar', 50);
+$dbc->defineField( 'State', 'varchar', 50);
+$dbc->defineField( 'Postcode', 'varchar', 10);
+$dbc->defineField( 'ConsolePropertyID', 'bigint');
+$dbc->defineField( 'Bond', 'float', 18, 3);
+$dbc->defineField( 'Rent', 'float', 18, 3);
+$dbc->defineField( 'Credit', 'float', 18, 3);
+$dbc->defineField( 'Period', 'int');
+$dbc->defineField( 'PaidTo', 'date');
+$dbc->defineField( 'LeaseFirstStart', 'date');
+$dbc->defineField( 'LeaseStart', 'date');
+$dbc->defineField( 'LeaseStop', 'date');
+$dbc->defineField( 'LeaseTerm', 'varchar', 50);
+$dbc->defineField( 'Vacating', 'date');
+$dbc->defineField( 'Inactive', 'tinyint');
+$dbc->defineField( 'Seen', 'tinyint');
+$dbc->defineField( 'Key', 'varchar', 50);
+$dbc->defineField( 'GUID', 'varchar', 50);
+$dbc->defineField( 'version', 'bigint');
+$dbc->defineField( 'LateSMSSent', 'datetime');
+$dbc->defineField( 'rental_variation_status', 'int');
+$dbc->defineField( 'rental_variation_status_updated', 'datetime');
+$dbc->defineField( 'rental_variation_status_updated_by', 'int');
+
+$dbc->defineIndex('LeaseStop', 'LeaseStop' );
+$dbc->defineIndex('GUID', 'GUID' );
+$dbc->defineIndex('ConsolePropertyID', 'ConsolePropertyID' );
+$dbc->defineIndex('version', 'version' );
+
+$dbc->check();
